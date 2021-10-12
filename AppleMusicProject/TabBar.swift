8cfc9fb049b0e3a0c5ceb1baa922841b9e8ca6bd
Oleg Kirsanov <olegkirsanov22@gmail.com>
2021-10-12 16:01:17 +0300

    Create TabBar

---
+++ AppleMusicProject/TabBar.swift
@@ -0,0 +1,57 @@
+//
+//  TabBar.swift
+//  AppleMusicProject
+//
+//  Created by Oleg Kirsanov on 12.10.2021.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    @State var current = 0
+    
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom), content: {
+            TabView(selection: $current) {
+                
+                // Таб Медиатека
+                LibraryView()
+                .tag(0)
+                .tabItem { Image(systemName: "music.note.house.fill")
+                    Text("Медиатека")
+                }
+                
+                // Таб Радио
+                NavigationView {
+                    ScrollView {
+                    }
+                        .navigationBarTitle("Радио")
+                }
+                .tag(1)
+                .tabItem { Image(systemName: "antenna.radiowaves.left.and.right")
+                    Text("Радио")
+                }
+                
+                // Таб Поиск
+                NavigationView {
+                    ScrollView {
+                    }
+                        .navigationBarTitle("Поиск")
+                }
+                .tag(2)
+                .tabItem { Image(systemName: "magnifyingglass")
+                    Text("Поиск")
+                }
+            }
+            .accentColor(.red)
+        
+        })
+    }
+}
+
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
