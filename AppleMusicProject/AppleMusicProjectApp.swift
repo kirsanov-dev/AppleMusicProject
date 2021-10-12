4019e946da654f053309a01483e21ead67fe8e55
Oleg Kirsanov <olegkirsanov22@gmail.com>
2021-10-12 13:38:06 +0300

    Initial Commit

---
+++ AppleMusicProject/AppleMusicProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicProjectApp.swift
+//  AppleMusicProject
+//
+//  Created by Oleg Kirsanov on 12.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
