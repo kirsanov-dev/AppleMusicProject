eb122ea9e52955d9f2f088b584120440501759d0
Oleg Kirsanov <olegkirsanov22@gmail.com>
2021-10-12 15:50:29 +0300

    Add a list to EditLibraryView, create a custom list item

--- AppleMusicProject/EditLibraryView.swift
+++ AppleMusicProject/EditLibraryView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct EditLibraryView: View {
+    @State var listItems = Model.data
+    @State var selections: [String] = []
+    @State var multiSelection = Set<UUID>()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            ForEach(listItems, id: \.id) { item in
+                ListItem(title: item.title, icon: item.icon, isSelected: self.selections.contains(item.title)){
+                    if self.selections.contains(item.title) {
+                        self.selections.removeAll(where: { item.title == $0 })
+                    } else {
+                        self.selections.append(item.title)
+                    }
+                }
+            }
+        }
+        .accentColor(.red)
     }
 }
+
+struct ListItem: View {
+    var title: String
+    var icon: String
+    var isSelected: Bool
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: self.action) {
+            HStack(spacing: 20) {
+                Image(systemName: self.icon)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.red)
+                Text(self.title)
+                    .font(.system(size: 24))
+            }
+            .frame(height: 58)
+        }
+    }
+}
+
 
 struct EditLibraryView_Previews: PreviewProvider {
     static var previews: some View {
